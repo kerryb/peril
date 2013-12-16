e72e86f6f2c73e521b1c9be6ca6e789a8687dd1a
Kerry Buckley <kerryjbuckley@gmail.com>
2013-12-16 11:47:56 +0000

    Add dojo games

---
+++ games/tuesday_dojo.rb
@@ -0,0 +1,167 @@
+game "Tuesday Dojo"
+
+players "Team 1", "Team 2", "Team 3"
+
+rewards 200, 400, 600, 800, 1_000
+
+category "Java"
+answer <<END_ANSWER, <<END_QUESTION
+This character can be found after most of lines of Java code.
+END_ANSWER
+What is a semi-colon?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This method creates an instance of a class.
+END_ANSWER
+What is a constructor?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+These are the modifiers that have to precede the declaration of the main method in a Java program.
+END_ANSWER
+What is public static void?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is the most common implementation of the List interface, and is usually preferable to primitive arrays.
+END_ANSWER
+What is ArrayList?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This library provides vastly superior date and time handling for Java 7 and below.
+END_ANSWER
+What is Joda Time?
+END_QUESTION
+
+category "Web"
+answer <<END_ANSWER, <<END_QUESTION
+This is the technology used to style web pages.
+END_ANSWER
+What is CSS?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This HTTP response code is returned when a page cannot be found.
+END_ANSWER
+What is 404?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This well-known web server shares its name with a Native American tribe.
+END_ANSWER
+What is Apache?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This technique uses Javascript to fetch data without reloading a page.
+END_ANSWER
+What is Ajax?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This security feature prevents scripts on one site accessing content on another.
+END_ANSWER
+What is same origin policy?
+END_QUESTION
+
+category "Design"
+answer <<END_ANSWER, <<END_QUESTION
+This principle reminds us to avoid speculative coding.
+END_ANSWER
+What is YAGNI?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This acronym warns us not to do the same thing at multiple places in our code.
+END_ANSWER
+What is DRY?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Tony Hoare described this as "my billion dollar mistake".
+END_ANSWER
+What is null?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This design pattern provides an interface for creating related objects without specifying their concrete classes.
+END_ANSWER
+What is the abstract factory pattern?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This term describes how well closely-related concepts are grouped together in the code.
+END_ANSWER
+What is cohesion?
+END_QUESTION
+
+category "People"
+answer <<END_ANSWER, <<END_QUESTION
+This GPO engineer designed the first  electronic programmable computer to help crack German codes in WW2.
+END_ANSWER
+Who is Tommy Flowers?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+He created a now-ubiquitous open source Unix-like operating system.
+END_ANSWER
+Who is Linus Torvalds?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+An agile manifesto signatory, he invented Extreme Programming and TDD.
+END_ANSWER
+Who is Kent Beck?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Daughter of the poet Lord Byron, this woman is credited with being the first computer programmer.
+END_ANSWER
+Who is Ada Lovelace?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+A US admiral and an early computer programmer, she wrote the first compiler and helped create COBOL.
+END_ANSWER
+Who is Grace Hopper?
+END_QUESTION
+
+category "TDD/Dojo"
+answer <<END_ANSWER, <<END_QUESTION
+This is the practice of having two people collaborate on one computer on a piece of code.
+END_ANSWER
+What is pair programming?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+These are the phases of the TDD cycle.
+END_ANSWER
+What is Red-Green-Refactor?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is a short practice exercise that we work to find a solution for in a dojo.
+END_ANSWER
+What is a kata?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is when you improve the design of a piece of code without changing its behaviour.
+END_ANSWER
+What is refactoring?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+These both stand in for real objects in tests, but one also verifies which methods were called
+END_ANSWER
+What are mocks and stubs?
+END_QUESTION
+
+category "Off-Topic"
+answer <<END_ANSWER, <<END_QUESTION
+This person was the director of The Lord of the Rings trilogy.
+END_ANSWER
+Who is Peter Jackson?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Traditionally this is the final day of the Christmas period when all Christmas decorations should be taken down.
+END_ANSWER
+What is January 6th (or Twelfth Night)?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is the original colour of Santa's suit.
+END_ANSWER
+What is green?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This what my true love gave to me on the ninth day.
+END_ANSWER
+What is 9 Ladies Dancing?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This thespian's major Hollywood film debut was in Die Hard in 1988.
+END_ANSWER
+Who is Alan Rickman?
+END_QUESTION
