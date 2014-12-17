0382a6ce16c208a663736e05df9c133966de0382
Kerry Buckley <kerryjbuckley@gmail.com>
2014-12-17 18:05:17 +0000

    Add 2014 dojo game

---
+++ games/dojo_2014.rb
@@ -0,0 +1,167 @@
+game "Dojo 2014"
+
+players "Team 1", "Team 2", "Team 3"
+
+rewards 200, 400, 600, 800, 1_000
+
+category "Programming"
+answer <<END_ANSWER, <<END_QUESTION
+This error, often caused by infinite recursion, gives its name to a popular programmers&rsquo; Q&A forum.
+END_ANSWER
+What is stack overflow?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This greeting is often produced by the first program written by someone learning a new language.
+END_ANSWER
+What is &ldquo;hello world&rdquo;?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Despite the sound of its name, this language is used by statisticians, not pirates.
+END_ANSWER
+What is R?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+In javascript, this is the identity operator; in ruby, it&rsquo;s used for case equality.
+END_ANSWER
+What is ===?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This inefficient sorting algorithm operates by repeatedly swapping successive elements it they are in the wrong order.
+END_ANSWER
+What is bubble sort?
+END_QUESTION
+
+category "Computer science"
+answer <<END_ANSWER, <<END_QUESTION
+This is used to describe the behaviour of algorithms as input size increases.
+END_ANSWER
+What is big-O notation?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This property of a program refers to how well related concepts are grouped together.
+END_ANSWER
+What is cohesion?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+These computation units always return the same value for given inputs, and have no side-effects.
+END_ANSWER
+What are pure functions?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This structure is often used as an intermediate representation between parsing source code and compilation.
+END_ANSWER
+What is an abstract syntax tree?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Alan Turing proved that there&rsquo;s no general solution to this problem, of determining whether a program will complete.
+END_ANSWER
+What is the halting problem?
+END_QUESTION
+
+category "BT History"
+answer <<END_ANSWER, <<END_QUESTION
+In 1971 a bomb exploded on the 31st floor of this building, famous at the time for its revolving restaurant.
+END_ANSWER
+What is the BT Tower?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This bird, voiced by Bernard Cribbins, told people to &ldquo;make someone happy with a phone call&rdquo;.
+END_ANSWER
+What is Buzby?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This core feature of the world-wide web was the subject of an infamous court case in 2000, when BT sued Prodigy Internet for patent infringement.
+END_ANSWER
+What are hyperlinks?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Until 1998, BT were not allowed to operate in this city.
+END_ANSWER
+What is [Kingston upon] Hull?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This policy, introduced in 2000, allows other operators to provide services using BT infrastructure between the customer premises and exchange.
+END_ANSWER
+What is local loop unbundling?
+END_QUESTION
+
+category "People"
+answer <<END_ANSWER, <<END_QUESTION
+His difference engine is famed for being the first mechanical computer.
+END_ANSWER
+Who is Charles Babbage?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+After single-handedly designing the Apple I in 1976, he co-founded a company to sell it.
+END_ANSWER
+Who is Steve Wozniak (Woz)?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+He is the K in both the unix program awk, and the C reference book K&R.
+END_ANSWER
+Who is Brian Kernighan?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+A co-founder of Intel, he is best known for an observation about the rate of increase in transistor density in integrated circuits.
+END_ANSWER
+who is Gordon Moore?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This agile manifesto signatory is best known for inventing the wiki.
+END_ANSWER
+Who is Ward Cunningham?
+END_QUESTION
+
+category "Internet Culture"
+answer <<END_ANSWER, <<END_QUESTION
+This artist&rsquo;s music video overflowed the YouTube view counter recently.
+END_ANSWER
+Who is Psy?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This actor was the first to hit one million followers on Twitter.
+END_ANSWER
+who is Ashton Kutcher?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+A scene from this film has been re-subtitled thousands of times to show Hitler ranting about various subjects
+END_ANSWER
+What is Downfall?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is the year Google was founded, XML was first published as a W3C recommendation, and Apple introduced the iMac.
+END_ANSWER
+What is 1998?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This website was famous for its animations of characters like Magical Trevor and Catface.
+END_ANSWER
+What is weebls stuff?
+END_QUESTION
+
+category "Christmas"
+answer <<END_ANSWER, <<END_QUESTION
+This is what my true love gave to me on the fifth day of Christmas.
+END_ANSWER
+What is 5 goo-oold rings?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This parasitic plant, with white berries, is the subject of a Christmas tradition.
+END_ANSWER
+What is mistletoe?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+These two reindeer take their names from foreign words for a weather phenomenon.
+END_ANSWER
+Who are Donner and Blitzen?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This was the first ghost to visit Ebenezer Scrooge in A Christmas Carol.
+END_ANSWER
+What is Marley&rsquo;s ghost?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+In European countries, this date is the feast of St Nicholas, and is celebrated by giving gifts.
+END_ANSWER
+What is 6 December?
+END_QUESTION
