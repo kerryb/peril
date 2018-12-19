bebe5ea07ce73c276970501c96030420e8fe8c33
Kerry Buckley <kerryjbuckley@gmail.com>
2018-12-19 16:19:38 +0000

    Add Nimboids Christmas 2018 team meeting game

---
+++ games/nimboids_2018.rb
@@ -0,0 +1,167 @@
+game "Nimboids Team Meeting"
+
+players "Team 1", "Team 2", "Team 3"
+
+rewards 200, 400, 600, 800, 1_000
+
+category "Ruby"
+answer <<END_ANSWER, <<END_QUESTION
+This statement creates a getter and setter for an instance variable
+END_ANSWER
+What is attr_accessor?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This gets called if an object receives a message it doesn’t have a method for
+END_ANSWER
+What is method_missing?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This tool is on the cover of one of the first books about Ruby to be published in English
+END_ANSWER
+What is the pickaxe?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This method applies a function to an array of values, returning an array of results with nesting removed
+END_ANSWER
+What is flat_map?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This file, an elegant/terrible hack, finally disappeared this year, but hasn’t been needed since Ruby shipped with RubyGems built-in
+END_ANSWER
+What is ubygems.rb?
+END_QUESTION
+
+category "Elixir"
+answer <<END_ANSWER, <<END_QUESTION
+This tool is used for compiling, running tests, managing dependencies and more
+END_ANSWER
+What is mix?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This atom is often returned in a tuple to indicate success
+END_ANSWER
+What is :ok?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This operator is used to concatenate two strings/binaries
+END_ANSWER
+What is <>?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This behaviour, a specialisation of GenServer, is used for simple process that store state
+END_ANSWER
+What is Agent?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+When used with supervision trees, this philosophy can counterintuitively lead to highly reliable distributed systems
+END_ANSWER
+What is “let it crash”?
+END_QUESTION
+
+category "Programming"
+answer <<END_ANSWER, <<END_QUESTION
+This error, often caused by infinite recursion, gives its name to a popular programmers’ Q&A forum
+END_ANSWER
+What is stack overflow?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This greeting is often produced by the first program written by someone learning a new language
+END_ANSWER
+What is “hello world”?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Despite the sound of its name, this language is used by statisticians, not pirates
+END_ANSWER
+What is R?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is used to check identity in Javascript, case equality in Ruby, and strict equality in Elixir
+END_ANSWER
+What is ===?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This inefficient sorting algorithm operates by repeatedly swapping successive elements it they are in the wrong order
+END_ANSWER
+What is bubble sort?
+END_QUESTION
+
+category "BT History"
+answer <<END_ANSWER, <<END_QUESTION
+In 1971 a bomb exploded on the 31st floor of this building, famous at the time for its revolving restaurant
+END_ANSWER
+What is the BT Tower?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This bird, voiced by Bernard Cribbins, told people to “make someone happy with a phone call”
+END_ANSWER
+What is Buzby?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This core WWW feature was the subject of an infamous court case in 2000, when BT sued Prodigy Internet for patent infringement
+END_ANSWER
+What are hyperlinks?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This was replaced by the ‘connected world’ logo in 2003
+END_ANSWER
+What was the BT Piper?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Until 1998, BT were not allowed to operate in this city
+END_ANSWER
+What is [Kingston upon] Hull?
+END_QUESTION
+
+category "People"
+answer <<END_ANSWER, <<END_QUESTION
+His difference engine is considered to be the first mechanical computer
+END_ANSWER
+Who is Charles Babbage?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+After single-handedly designing the Apple I in 1976, he co-founded a company to sell it
+END_ANSWER
+Who is Steve Wozniak (Woz)?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+He is the K in both the unix program awk, and the C reference book K&R
+END_ANSWER
+Who is Brian Kernighan?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+A co-founder of Intel, he is best known for an observation about the rate of increase in transistor density in integrated circuits
+END_ANSWER
+who is Gordon Moore?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This agile manifesto signatory is best known for inventing the wiki
+END_ANSWER
+Who is Ward Cunningham?
+END_QUESTION
+
+category "Lucky Dip"
+answer <<END_ANSWER, <<END_QUESTION
+This inanimate object can provide a surprisingly helpful ear when you’re stuck on a programming problem
+END_ANSWER
+What is a rubber duck?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This game, usually played with one or two players per side, is regulated by the ITSF
+END_ANSWER
+What is foosball/table football?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This law states that a robot may not injure a human being or, through inaction, allow a human being to come to harm
+END_ANSWER
+What is the first law of robotics?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is a recursive question
+END_ANSWER
+What is a recursive question?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+During the construction of the great pyramids, each worker received a daily ration of about a gallon of this beverage
+END_ANSWER
+What is beer?
+END_QUESTION
